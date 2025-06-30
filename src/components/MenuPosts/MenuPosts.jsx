447fc8d78bc0a23b33bbd752b8d450ee51b975d6
mrez09 <mrizkiekasaputra@gmail.com>
2025-06-30 09:47:33 +0700

    Popular post dalam rentang 1 bulan.

--- src/components/MenuPosts/MenuPosts.jsx
+++ src/components/MenuPosts/MenuPosts.jsx
@@ -2,99 +2,51 @@ import React from "react";
 import styles from "./menuPosts.module.css";
 import Link from "next/link";
 import Image from "next/image";
+import WaktuUpload from "../time/waktuUpload";
 
-const MenuPosts = ({ withImage }) => {
+const getData = async (page, cat) => {
+  const waktuUnggah = new Date(); // Contoh: Waktu unggah saat ini
+  const res = await fetch("http://localhost:3000/api/popular", {
+    cache: "no-store",
+  });
+
+  if (!res.ok) {
+    throw new Error("Failed");
+  }
+
+  return res.json();
+};
+
+const MenuPosts = async ({ withImage }) => {
+  const data = await getData();
+
   return (
     <div className={styles.items}>
-      <Link className={styles.item} href="/">
-        {withImage && (
-          <div className={styles.imageContainer}>
-            <Image
-              className={styles.image}
-              src="/samp1.png"
-              alt="sample 1"
-              fill
-            />
+      {data?.map((item) => (
+        <Link className={styles.item} href="/">
+          {withImage && (
+            <div className={styles.imageContainer}>
+              <Image
+                className={styles.image}
+                src={item.image}
+                alt={item.title}
+                fill
+              />
+            </div>
+          )}
+          <div className={styles.textContainer}>
+            <span className={`${styles.category} ${styles.html}`}>HTML</span>
+            <h3 className={styles.postTitle}>{item.title}</h3>
+            <div className={styles.detail}>
+              <span className={styles.username}>{item.user.name} - </span>
+              <span className={styles.date}>
+                <WaktuUpload waktuUnggah={item.createdAt} />
+              </span>
+              <p className={styles.date}> dilihat {item.views}</p>
+            </div>
           </div>
-        )}
-        <div className={styles.textContainer}>
-          <span className={`${styles.category} ${styles.html}`}>HTML</span>
-          <h3 className={styles.postTitle}>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-          </h3>
-          <div className={styles.detail}>
-            <span className={styles.username}>MreZ - </span>
-            <span className={styles.date}>20-06-2025</span>
-          </div>
-        </div>
-      </Link>
-      <Link className={styles.item} href="/">
-        {withImage && (
-          <div className={styles.imageContainer}>
-            <Image
-              className={styles.image}
-              src="/samp1.png"
-              alt="sample 1"
-              fill
-            />
-          </div>
-        )}
-        <div className={styles.textContainer}>
-          <span className={`${styles.category} ${styles.tech}`}>Tech</span>
-          <h3 className={styles.postTitle}>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-          </h3>
-          <div className={styles.detail}>
-            <span className={styles.username}>MreZ - </span>
-            <span className={styles.date}>20-06-2025</span>
-          </div>
-        </div>
-      </Link>
-      <Link className={styles.item} href="/">
-        {withImage && (
-          <div className={styles.imageContainer}>
-            <Image
-              className={styles.image}
-              src="/samp1.png"
-              alt="sample 1"
-              fill
-            />
-          </div>
-        )}
-        <div className={styles.textContainer}>
-          <span className={`${styles.category} ${styles.react}`}>React</span>
-          <h3 className={styles.postTitle}>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-          </h3>
-          <div className={styles.detail}>
-            <span className={styles.username}>MreZ - </span>
-            <span className={styles.date}>20-06-2025</span>
-          </div>
-        </div>
-      </Link>
-      <Link className={styles.item} href="/">
-        {withImage && (
-          <div className={styles.imageContainer}>
-            <Image
-              className={styles.image}
-              src="/samp1.png"
-              alt="sample 1"
-              fill
-            />
-          </div>
-        )}
-
-        <div className={styles.textContainer}>
-          <span className={`${styles.category} ${styles.coding}`}>Coding</span>
-          <h3 className={styles.postTitle}>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-          </h3>
-          <div className={styles.detail}>
-            <span className={styles.username}>MreZ - </span>
-            <span className={styles.date}>20-06-2025</span>
-          </div>
-        </div>
-      </Link>
+        </Link>
+      ))}
     </div>
   );
 };
