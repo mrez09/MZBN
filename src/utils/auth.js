803d958cc48f10eb5e803c73cd48d837972c6c97
mrez09 <mrizkiekasaputra@gmail.com>
2025-07-05 17:08:18 +0700

    test vercel

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,5 +1,5 @@
 import { PrismaAdapter } from "@auth/prisma-adapter";
-import GithubProvider from "next-auth/providers/github";
+//import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
 import prisma from "./connect";
 import { getServerSession } from "next-auth/next";
