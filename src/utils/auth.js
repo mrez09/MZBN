0879b0936a0844863ed30020022c00481b741338
mrez09 <mrizkiekasaputra@gmail.com>
2025-07-05 16:48:39 +0700

    Vercel Auth error

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -2,7 +2,7 @@ import { PrismaAdapter } from "@auth/prisma-adapter";
 import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
 import prisma from "./connect";
-import { getServerSession } from "next-auth";
+import { getServerSession } from "next-auth/next";
 
 export const authOptions = {
   adapter: PrismaAdapter(prisma),
@@ -12,10 +12,12 @@ export const authOptions = {
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_SECRET,
     }),
+    /*
     GithubProvider({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
     }),
+    */
   ],
 };
 
