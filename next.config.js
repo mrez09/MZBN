21390fed168017370638fe90b6071dee79db54af
mrez09 <mrizkiekasaputra@gmail.com>
2025-07-07 05:45:52 +0700

    test vercel 1.6

--- next.config.js
+++ next.config.js
@@ -3,9 +3,9 @@ const nextConfig = {
   images: {
     domains: ["lh3.googleusercontent.com", "ik.imagekit.io"],
   },
-  //  api: {
-  //    bodyParser: false,
-  //  },
+  api: {
+    bodyParser: false,
+  },
   eslint: {
     // ❗Ini yang penting: lewati error ESLint saat build (misal di Vercel)
     ignoreDuringBuilds: true,
