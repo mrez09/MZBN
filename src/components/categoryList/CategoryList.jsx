4f69c9659124755a92ed5d0e7ea77c648c60297d
mrez09 <mrizkiekasaputra@gmail.com>
2025-07-07 06:56:46 +0700

    Backup versi async)

--- src/components/categoryList/CategoryList.jsx
+++ src/components/categoryList/CategoryList.jsx
@@ -3,16 +3,17 @@ import React from "react";
 import styles from "./categoryList.module.css";
 import Link from "next/link";
 import Image from "next/image";
-import prisma from "@/utils/connect";
 
 const getData = async () => {
-  const categories = await prisma.category.findMany({
-    take: 6,
-    orderBy: {
-      id: "desc",
-    },
+  const res = await fetch("http://localhost:3000/api/categories", {
+    cache: "no-store",
   });
-  return categories;
+
+  if (!res.ok) {
+    throw new Error("Failed");
+  }
+
+  return res.json();
 };
 
 const CategoryList = async () => {
