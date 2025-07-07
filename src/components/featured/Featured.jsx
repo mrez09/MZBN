5058dec5127eead9d66ed049edaba05ced1bbf22
mrez09 <mrizkiekasaputra@gmail.com>
2025-07-07 08:07:45 +0700

    test vercel 1.21 (get data dulu)

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -1,48 +1,56 @@
-"use client";
-import React, { useEffect, useState } from "react";
+import React from "react";
 import styles from "./featured.module.css";
 import Image from "next/image";
 import Link from "next/link";
 
-const Featured = () => {
-  const [data, setData] = useState([]);
+//const getData = async (page, cat) => {
+//const waktuUnggah = new Date(); // Contoh: Waktu unggah saat ini
+//const res = await fetch("http://localhost:3000/api/featured", {
+//  cache: "no-store",
+//});
 
-  useEffect(() => {
-    fetch(`${process.env.NEXT_PUBLIC_SITE_URL}/api/featured`)
-      .then((res) => res.json())
-      .then(setData)
-      .catch((err) => console.error("Fetch error:", err));
-  }, []);
+//if (!res.ok) {
+//  throw new Error("Failed");
+//}
+
+//return res.json();
+//};
+
+const Featured = async () => {
+  //const data = await getData();
 
   return (
     <>
-      {data?.map((item) => (
-        <div className={styles.container} key={item.id}>
-          <h1 className={styles.title}>
-            <b>Hey, Iam MreZ Here!</b> Discover my stories and creative ideas.
-          </h1>
-          <div className={styles.post}>
-            <div className={styles.imgContainer}>
-              <Image className={styles.image} src={item.image} alt="" fill />
-            </div>
-            <div className={styles.textContainer}>
-              <h1 className={styles.postTitle}>{item.title}</h1>
-              <span className={`${styles.categoryItem} ${styles[item.slug]}`}>
-                {item.cat.title}
-              </span>
-              <div
-                className={styles.description}
-                dangerouslySetInnerHTML={{
-                  __html: item.desc.substring(0, 150),
-                }}
-              />
-              <Link href={`/posts/${item.slug}`}>
-                <button className={styles.button}>Read More</button>
-              </Link>
+      {/*data?.map((item) => (
+        <React.Fragment key={item.id}>
+          <div className={styles.container}>
+            <h1 className={styles.title}>
+              <b>Hey, Iam MreZ Here!</b> Discover my stories and creative ideas.
+            </h1>
+
+            <div className={styles.post}>
+              <div className={styles.imgContainer}>
+                <Image className={styles.image} src={item.image} alt="" fill />
+              </div>
+              <div className={styles.textContainer}>
+                <h1 className={styles.postTitle}>{item.title}</h1>
+                <span className={`${styles.categoryItem} ${styles[item.slug]}`}>
+                  {item.cat.title}
+                </span>
+                <div
+                  className={styles.description}
+                  dangerouslySetInnerHTML={{
+                    __html: item.desc.substring(0, 150),
+                  }}
+                />
+                <Link href={`/posts/${item.slug}`}>
+                  <button className={styles.button}>Read More</button>
+                </Link>
+              </div>
             </div>
           </div>
-        </div>
-      ))}
+        </React.Fragment>
+      ))*/}
     </>
   );
 };
