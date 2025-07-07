3a6aa72ec6c0dc371fd7d56157594464e6723f37
mrez09 <mrizkiekasaputra@gmail.com>
2025-07-07 07:33:12 +0700

    test vercel 1.14

--- src/components/comments/Comments.jsx
+++ src/components/comments/Comments.jsx
@@ -23,7 +23,7 @@ const Comments = ({ postSlug }) => {
   const { status } = useSession();
 
   const { data, mutate, isLoading } = useSWR(
-    `http://localhost:3000/api/comments?postSlug=${postSlug}`,
+    `${process.env.NEXT_PUBLIC_SITE_URL}/api/comments?postSlug=${postSlug}`,
     fetcher
   );
 
