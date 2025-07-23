a388ab779efeea619e54ba0c0981c9e3c3efb60a
mrez09 <mrizkiekasaputra@gmail.com>
2025-07-23 23:04:21 +0700

    vercel -__-

--- src/utils/connect.js
+++ src/utils/connect.js
@@ -2,8 +2,8 @@ import { PrismaClient } from "@prisma/client";
 
 const globalForPrisma = globalThis;
 
-const prisma = globalForPrisma.prisma ?? new PrismaClient();
+const prisma = globalForPrisma.prisma || new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
-export default prisma; // ✅ default export
+export default prisma;
