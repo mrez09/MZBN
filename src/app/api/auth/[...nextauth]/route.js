4682acea4aff90fe719ebc50242bdaf06eaff9fe
mrez09 <mrizkiekasaputra@gmail.com>
2025-07-23 20:20:53 +0700

    repair auth js

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth";
-import authOptions from "@/utils/auth";
+//import authOptions from "@/utils/auth";
+import { authOptions } from "@/utils/auth";
 
 // ⛔ Mencegah eksekusi saat build static
 export const dynamic = "force-dynamic";
