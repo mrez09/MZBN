38c42fdaed3a82809dcc9d9698a71d216cace99c
mrez09 <mrizkiekasaputra@gmail.com>
2025-07-23 20:24:42 +0700

    buang dynamic

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,9 +1,5 @@
 import NextAuth from "next-auth";
-//import authOptions from "@/utils/auth";
 import { authOptions } from "@/utils/auth";
-
-// ⛔ Mencegah eksekusi saat build static
-export const dynamic = "force-dynamic";
 
 const handler = NextAuth(authOptions);
 
